0862a910afd9affd438c0dccf06feebd6aedf430
khamid343 <47668793+khamid343@users.noreply.github.com>
2026-06-13 09:50:56 +0700

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,9 +6,6 @@ import 'examples/map_horizontal_example.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
-  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-
   runApp(const MyApp());
 }
 
@@ -21,26 +18,16 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MapHorizontalExample(),
-      //home: MapVerticalExample(),
+      //home: MapHorizontalExample(),
+      home: MapVerticalExample(),
     );
   }
 }
